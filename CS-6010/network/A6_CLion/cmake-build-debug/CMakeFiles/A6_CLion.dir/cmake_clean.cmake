d283935891c87724aa6e17301a2ca75ddeddd460
SAAllegri <52748575+SAAllegri@users.noreply.github.com>
2021-02-09 23:39:54 -0500

    Add files via upload

---
+++ CS-6010/network/A6_CLion/cmake-build-debug/CMakeFiles/A6_CLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A6_CLion.exe"
+  "A6_CLion.exe.manifest"
+  "A6_CLion.pdb"
+  "CMakeFiles/A6_CLion.dir/network.c.obj"
+  "libA6_CLion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A6_CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
