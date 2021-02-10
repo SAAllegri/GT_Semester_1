37599cbcbe42dc8a15e777de0734eb73f320b7d5
SAAllegri <52748575+SAAllegri@users.noreply.github.com>
2021-02-09 23:13:35 -0500

    Add files via upload

---
+++ Assignment_1/A1_CLion/cmake-build-debug/CMakeFiles/A1_CLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A1_CLion.exe"
+  "A1_CLion.exe.manifest"
+  "A1_CLion.pdb"
+  "CMakeFiles/A1_CLion.dir/powerlaw.c.obj"
+  "libA1_CLion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A1_CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
